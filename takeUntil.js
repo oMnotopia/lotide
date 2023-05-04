57ed75dd1eb34909fe4513f102feab0eac0a081b
Clayton <persinger.clayton@gmail.com>
2023-05-04 14:31:44 -0700

    removed blank space

--- takeUntil.js
+++ takeUntil.js
@@ -21,3 +21,4 @@ console.log('---');
 const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
 const results2 = takeUntil(data2, x => x === ',');
 console.log(results2);
+
