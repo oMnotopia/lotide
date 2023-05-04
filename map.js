42154aac67593c9f49d8b9704a6432445f960e87
Clayton <persinger.clayton@gmail.com>
2023-05-04 14:03:51 -0700

    implementation of the standard map() function

---
+++ map.js
@@ -0,0 +1,31 @@
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) return;
+
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) return false;
+  }
+  return true;
+};
+
+const assertArrayEqual = function(arr1, arr2) {
+  if (eqArrays(arr1, arr2)) return console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`);
+  return console.log(`❌❌❌ Assertion Failed: ${arr1} !== ${arr2}`);
+};
+
+const map = (arr, callback) => {
+  const resultingArr = [];
+
+  for (const item of arr) {
+    resultingArr.push(callback(item));
+  }
+
+  return resultingArr;
+};
+
+const words = ["ground", "control", "to", "major", "tom"];
+
+const results1 = map(words, word => word[0]);
+const results2 = map(words, () => words[0]);
+
+assertArrayEqual(results1, ['g', 'c', 't', 'm', 't']);
+assertArrayEqual(results2, ["ground", "ground", "ground", "ground", "ground"]);
