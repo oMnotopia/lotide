27c70613fbd0f425b79843c7d71ab203b364ae62
Clayton <persinger.clayton@gmail.com>
2023-05-06 20:12:18 -0700

    Update function to work recursively in situation where there are nested arrays.

--- eqArrays.js
+++ eqArrays.js
@@ -9,9 +9,14 @@ const eqArrays = function(arr1, arr2) {
   if (arr1.length !== arr2.length) return;
 
   for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) return false;
+    console.log(arr1[i], arr2[i]);
+    if (Array.isArray(arr1[i])) {
+      if (!eqArrays(arr1[i], arr2[i])) return false;
+    }
+
+    if (arr1[i] !== arr2[i] && !Array.isArray(arr1[i])) return false;
   }
   return true;
 };
 
-assertEqual(eqArrays([], [1, 2, 3]), true); // => should PASS
+assertEqual(eqArrays([[[[2, 3], [["hi"]]]]], [[[[2, 3], [["hi"]]]]]), true); // => should PASS
