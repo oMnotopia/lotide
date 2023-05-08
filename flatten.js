ce1625c5ea6c2470326d123f9f0a8e35546499b9
Clayton <persinger.clayton@gmail.com>
2023-05-08 13:32:03 -0700

    Fixed spacing issue.

--- flatten.js
+++ flatten.js
@@ -1,17 +1,3 @@
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length !== arr2.length) return;
-
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) return false;
-  }
-  return true;
-};
-
-const assertArrayEqual = function(arr1, arr2) {
-  if (eqArrays(arr1, arr2)) return console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`);
-  return console.log(`❌❌❌ Assertion Failed: ${arr1} !== ${arr2}`);
-};
-
 const flatten = function(arr) {
   let flattenedArray = [];
   for (let item of arr) {
