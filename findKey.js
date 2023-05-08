496abcabacef357f28dd17f5fa601d38f7f4b6a3
Clayton <persinger.clayton@gmail.com>
2023-05-08 13:22:54 -0700

    Removed test code to it's own file.

--- findKey.js
+++ findKey.js
@@ -1,10 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    return console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  }
-  return console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
-};
-
 const findKey = (obj, callback) => {
   for (const element in obj) {
     if (callback(obj[element])) {
@@ -14,20 +7,3 @@ const findKey = (obj, callback) => {
 };
 
 module.exports = findKey;
-
-assertEqual(findKey({
-  "Blue Hill": { stars: 1 },
-  "Akaleri":   { stars: 3 },
-  "noma":      { stars: 2 },
-  "elBulli":   { stars: 3 },
-  "Ora":       { stars: 2 },
-  "Akelarre":  { stars: 3 }
-}, x => x.stars === 2), "noma");// => "noma"
-
-assertEqual(findKey({
-  Vancouver: 55,
-  Princeton: 20,
-  Detroit: 80,
-  Banff: 100,
-  Miami:  25,
-}, x => x < 20), "Miami");
