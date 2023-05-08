9adb47c916631b96d573b75482d07a4a695fd1d4
Clayton <persinger.clayton@gmail.com>
2023-05-08 08:48:15 -0700

    Refactor code so it returns a value instead of calling assertEqual Function, then using the return value to test.

--- tail.js
+++ tail.js
@@ -1,17 +1,8 @@
 const tail = function(arr) {
   const arrTail = arr.slice(1);
   for (let i = 0; i < arrTail.length; i++) {
-    assertEqual(arrTail[i], arr[i + 1]);
+    return arrTail;
   }
 };
 
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    return console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  }
-  return console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
-};
-
-let j = [1, 2, 3, 4];
-
-console.log(tail(j));
+module.exports = tail;
