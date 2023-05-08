147d3bcabacdb0e189f64e595b332ab8e650cc07
Clayton <persinger.clayton@gmail.com>
2023-05-08 09:05:27 -0700

    Refactor code to export named function and removed unnecessary functions. Also, create a test file for named function.

---
+++ test/middleTest.js
@@ -0,0 +1,5 @@
+const assertArrayEqual = require("../assertArrayEqual");
+const findMiddleOfArray = require("../middle");
+
+//Test code
+assertArrayEqual(findMiddleOfArray([6,2,'hello',4,5,1]), ['hello',4]);
