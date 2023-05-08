cc2faec28713b71bf7d071ddf1a24f978fe0c267
Clayton <persinger.clayton@gmail.com>
2023-05-08 08:20:41 -0700

    Refactor code so that test funcitons is no longer copy pasted into other code and instead uses module.exports and require() to use the code. Also, created new folder to contain test code.

--- assertEqual.js
+++ assertEqual.js
@@ -5,8 +5,6 @@ const assertEqual = function(actual, expected) {
   return console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
 };
 
-//Test code
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual("Lighthouse Labs", "Lighthouse Labs");
-assertEqual(1, 1);
-assertEqual(1, -1);
+module.exports = assertEqual;
+
+
---
+++ test/assertEqualTest.js
@@ -0,0 +1,7 @@
+const assertEqual = require("../assertEqual");
+
+//Test code
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual("Lighthouse Labs", "Lighthouse Labs");
+assertEqual(1, 1);
+assertEqual(1, -1);
