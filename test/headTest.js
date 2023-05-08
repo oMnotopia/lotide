879d9725413f538ec37f7c6fcc0f882ad9156b66
Clayton <persinger.clayton@gmail.com>
2023-05-08 09:22:29 -0700

    Added Moch and Chai dependencies. Also, updated headTest to use Mocha and Chai for unit testing.

--- test/headTest.js
+++ test/headTest.js
@@ -1,6 +1,21 @@
 const assertEqual = require("../assertEqual");
 const head = require("../head");
+const assert = require("chai").assert;
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([]), "Hello");
+describe("#head", () => {
+  it("Returns 5 for [5, 6, 7]", () => {
+    assert.strictEqual(head([5, 6, 7]), 5);
+  });
+
+  it("Returns 5 for [5]", () => {
+    assert.strictEqual(head([5]), 5);
+  });
+
+  it("Returns 'Hello' from ['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.strictEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+  });
+
+  it("Returns undefined for []", () => {
+    assert.isUndefined(head([]));
+  });
+});
